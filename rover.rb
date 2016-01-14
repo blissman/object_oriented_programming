09f46a2f93c721512910f71ab0feb943c87982ab
Bliss Man <blissman@gmail.com>
2016-01-13 19:21:44 -0500

    write method to convert user input to arrays; get first two inputs

--- rover.rb
+++ rover.rb
@@ -1,12 +1,19 @@
+def convert_to_array(str)
+  str = str.split(" ")
+end
+
 class Rover
   attr_accessor :xcoordinate
   attr_accessor :ycoordinate
   attr_accessor :direction
 
   def read_instruction
+    #This method should take the input data and convert the strings input by
+    #the user to arrays. These arrays should then be used to compute the
+    #direction and position changes of each rover.
   end
 
-  def move
+  def move(direction)
     if direction == "N"
       ycoordinate += 1
     elsif direction == "E"
@@ -18,7 +25,7 @@ class Rover
       xcoordinate -= 1
   end
 
-  def turn(turn_instruction)
+  def turn(direction, turn_instruction)
     if direction == "N" && turn_instruction == "L"
       direction = "W"
     elsif
@@ -44,5 +51,19 @@ class Rover
       direction == "N"
     end
   end
-
 end
+
+Rover1 = Rover.new
+Rover2 = Rover.new
+
+puts "Please enter the plateau size (x,y):"
+plateau_size = gets.chomp
+convert_to_array(plateau_size)
+x_min = 0
+x_max = plateau_size[0]
+y_min = 0
+y_max = plateau_size[1]
+
+puts "Please enter Rover1's start position (x=#{x_min} to #{x_max}, y=#{y_min} to #{y_max}, start direction = N/E/S/W):"
+start_position1 = gets.chomp
+convert_to_array(start_position1)
